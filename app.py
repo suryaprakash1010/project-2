de5eea72a01e4efa9cb5f2f6807fff24de6e9df4
suryaprakash1010 <suryaprakashv@thestackly.com>
2026-09-08 14:10:18 +0530

    app.py

--- app.py
+++ app.py
@@ -470,7 +470,7 @@ def forecast_rows(base_rows, dates, manual_values=None):
             {
                 "Date": pd.Timestamp(d),
                 "Day": pd.Timestamp(d).day_name(),
-                "Week": ((pd.Timestamp(d).dayofyear - 1) // 7) + 1,,
+                "Week": ((pd.Timestamp(d).dayofyear - 1) // 7) + 1,
                 "Month": pd.Timestamp(d).month_name(),
                 "Year": pd.Timestamp(d).year,
                 "Forecast_Units": round(float(np.sum(pred)), 2)
